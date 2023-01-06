fea45f324f8109e27d74cb9cafd0f361db6dffca
sunhara <Tom_gaoshen@163.com>
2023-01-06 13:08:09 +0800

    Update Stoffemers Volume Apply for RPS.py

--- Stiffeners Volume Apply/Stoffemers Volume Apply for RPS.py
+++ Stiffeners Volume Apply/Stoffemers Volume Apply for RPS.py
@@ -16,23 +16,6 @@ from System.Collections.Generic import * #Lets you handle generics. Revit's API
 #sometimes wants hard-typed 'generic' lists, called ILists. If you don't need
 #these you can delete this line.
 
-clr.AddReference('ProtoGeometry')  #A Dynamo library for its proxy geometry
-#classes. You'll only need this if you're interacting with geometry.
-from Autodesk.DesignScript.Geometry import * #Loads everything in Dynamo's
-#geometry library
-clr.AddReference("RevitNodes") #Dynamo's nodes for Revit
-
-import Revit #Loads in the Revit namespace in RevitNodes
-clr.ImportExtensions(Revit.Elements) #More loading of Dynamo's Revit libraries
-clr.ImportExtensions(Revit.GeometryConversion) #More loading of Dynamo's
-#Revit libraries. You'll only need this if you're interacting with geometry.
-clr.AddReference("RevitServices") #Dynamo's classes for handling Revit documents
-
-import RevitServices 
-from RevitServices.Persistence import DocumentManager #An internal Dynamo class
-#that keeps track of the document that Dynamo is currently attached to
-from RevitServices.Transactions import TransactionManager #A Dynamo class for
-#opening and closing transactions to change the Revit document's database
 
 clr.AddReference("RevitAPI") #Adding reference to Revit's API DLLs
 clr.AddReference("RevitAPIUI") #Adding reference to Revit's API DLLs
@@ -41,13 +24,10 @@ import Autodesk #Loads the Autodesk namespace
 from Autodesk.Revit.DB import * #Loading Revit's API classes
 from Autodesk.Revit.UI import * #Loading Revit's API UI classes  
 
-#doc = DocumentManager.Instance.CurrentDBDocument 
 doc = __revit__.ActiveUIDocument.Document
 #__revit__ is a predefined variable 
 #A reference to the Autodesk.Revit.Application instance, obtained from the ExternalCommandData aurgument passed to plugins.
 #Finally, setting up handles to the active Revit document
-uiapp = DocumentManager.Instance.CurrentUIApplication
-#Setting a handle to the active Revit UI document
 
 app = __revit__.Application
 #Setting a handle to the currently-open instance of the Revit application
